3d80ff3832c8e6611593eaa096bef83ae2322f09
Exauc3 <ziakondayala@gmail.com>
2025-12-16 21:28:54 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const cacheName = 'artisanconnect-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
