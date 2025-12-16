c68f00c65a81be19634721fe442d92f6ad7964a7
Exauc3 <ziakondayala@gmail.com>
2025-12-16 22:02:08 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,35 @@
-const cacheName = 'artisanconnect-v1';
-const assets = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
+const cacheName = 'artisanconnect-cache-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
+// Installation et cache
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
+    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
   );
 });
 
+// Activation
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== cacheName) return caches.delete(key);
+      }));
+    })
+  );
+});
+
+// Fetch
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
